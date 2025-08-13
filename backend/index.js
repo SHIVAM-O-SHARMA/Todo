2e490c81b343f5e4155db4fd8646c218dec32df4
shivam <[shivamsharmas01230@gmail.com]>
2025-08-13 15:12:45 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.post('/auth/signin',async(req,res) => {
 try {
   const {email,password}= req.body;
   const user= await User.findOne({ email });
-  if(!user){return res.status(400).json({msg:'❌user does not exist'})};
+  if(!user){return res.status(400).json({msg:'❌user does not exist!'})};
 
   const isMatch= await bcrypt.compare(password, user.password);
   if(!isMatch){return res.status(400).json({msg:'invalid credentials'})};
